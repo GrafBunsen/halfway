5f377c29170c79301d43a83604ae85610d0af77c
Niklas <nlabitzke77@gmail.com>
2022-09-30 15:22:18 +0200

    feat: commit for daniel to show off :)

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -15,7 +15,8 @@ const response = ref();
 fetch(`http://localhost:3001/calculate/${startOfA?.stop_id}/${startOfB?.stop_id}`).then(
     async x => {
         result.value = true;
-        response.value = await x.json();
+        response.value = (await x.json()).middleStop.data.label;
+        //console.log(response.value.middleStop.data.label)
     }
 );
 
@@ -26,9 +27,10 @@ fetch(`http://localhost:3001/calculate/${startOfA?.stop_id}/${startOfB?.stop_id}
         <div v-if="!result">
             <LoadingResult></LoadingResult>
         </div>
-        <div v-else>
-            {{ response }}
-            {{ response.middleStop.label }}
+        <div v-else class="mx-8">
+            <p class="text-l mt-8 ">Trefft euch in:</p>
+            <p class="text-xl m-auto font-bold ">{{ response }}</p>
+            <p class="text-l mt-4 ">:)</p>
         </div>
         
   <footer class="absolute bottom-0 min-h-4 bg-[#A4FBD6] p-2 content-none w-full"></footer>
